15141f90d31434b7d485072235b4c294cc648200
Katipally Yashwanth Reddy <101495385+katipally@users.noreply.github.com>
2024-10-31 00:28:07 -0700

    Add files via upload

---
+++ build_mau/target/compiled/build_mau/models/schema.yml/not_null_session_summary_session_id.sql
@@ -0,0 +1,11 @@
+
+    
+    
+
+
+
+select session_id
+from dev.analytics.session_summary
+where session_id is null
+
+
---
+++ build_mau/target/run/build_mau/snapshots/snapshot_session_summary.sql
@@ -0,0 +1,36 @@
+
+      
+  
+    
+
+        create or replace transient table dev.snapshots.snapshot_session_summary
+         as
+        (
+    
+
+    select *,
+        md5(coalesce(cast(session_id as varchar ), '')
+         || '|' || coalesce(cast(end_time as varchar ), '')
+        ) as dbt_scd_id,
+        end_time as dbt_updated_at,
+        end_time as dbt_valid_from,
+        
+  
+  coalesce(nullif(end_time, end_time), null)
+  as dbt_valid_to
+
+    from (
+        
+
+
+
+select * from dev.analytics.session_summary
+
+    ) sbq
+
+
+
+        );
+      
+  
+  
